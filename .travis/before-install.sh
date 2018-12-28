f6109106eaa847426f07e8ad13aa38ea816c3bdc
Christos Tsakostas <c.tsakostas@gmail.com>
2018-12-28 18:59:01 +0200

    fix: travis condition for master branch - not pull request condition added

--- .travis/before-install.sh
+++ .travis/before-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ]; then
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_25e4e179b367_key -iv $encrypted_25e4e179b367_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
     gpg --fast-import .travis/codesigning.asc
 fi
